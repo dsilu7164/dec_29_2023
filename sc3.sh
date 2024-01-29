2f338723b462e513ae032df58697a6e4ea226228
dsilu7164 <dassilu323@gmail.com>
2024-01-29 13:42:17 +0530

    modified third file

--- sc3.sh
+++ sc3.sh
@@ -7,7 +7,10 @@ read a
 if [ $a -ge 13 -a $a -le 19 ];
 then
 	echo "teen ager"
-elif [ $a -ge 20 ];
+elif [ $a -ge 20  -a $a -le 60 ];
 then
 	echo "adult"
+elif [ $a -gt 60 ];
+then
+	echo "old age"
 fi
