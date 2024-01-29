c6a8b2d2d483f31a39b8cb7ba566bec693e6b525
dsilu7164 <dassilu323@gmail.com>
2024-01-29 12:50:05 +0530

    files added

---
+++ sc2.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+#
+#
+#
+#
+echo "enter your salary"
+read a
+if [ $a -lt 15000 ];
+then
+	echo "no tax"
+elif [ $a -gt 15000  -a  $a -le 30000 ];
+then
+	echo "5% tax"
+elif [ $a -gt 30000 ];
+then
+	echo "10% tax"
+fi
